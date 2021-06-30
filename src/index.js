c0b9135762220ff5c060515fa6738f618de3371d
Arti Gula <mister.gula@bk.ru>
2021-06-30 14:51:38 +0300

    tabs complected

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import modals from '../src/modules/modals';
+import tabs from '../src/modules/tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
+    tabs('.slider', '.slider-preview', '.slider-content-block', 'active');
 })
